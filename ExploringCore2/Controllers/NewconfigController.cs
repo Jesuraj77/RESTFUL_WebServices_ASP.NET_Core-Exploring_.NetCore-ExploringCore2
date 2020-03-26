60bea0a0116636f4635a8bb59a7905f6d79a6edf
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:33:26 +0530

    files added

---
+++ ExploringCore2/Controllers/NewconfigController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploringCore2.Controllers
+{
+    public class NewconfigController : Controller
+    {
+       // Enable Static File Rendering on the project Start up Class.
+
+
+        //Configure different Environments in the.Net Core Application -Development, Staging, Production.
+
+    }
+}
